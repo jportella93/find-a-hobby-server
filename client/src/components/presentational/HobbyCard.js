8834e5156e30600fa6ec07b1b172875521c69aeb
jportella93 <jportella93@gmail.com>
2018-06-06 20:47:02 +0200

    chore: add alt attr to imgs

--- client/src/components/presentational/HobbyCard.js
+++ client/src/components/presentational/HobbyCard.js
@@ -4,7 +4,8 @@ class HobbyCard extends Component {
   render() {
     return (
       <div className="HobbyCard">
-        <img src={this.props.hobby.pictures[0].big}/>
+        <img src={this.props.hobby.pictures[0].big}
+          alt={this.props.hobby.name}/>
         <h1>{this.props.hobby.name}</h1>
         <p>{this.props.hobby.description}</p>
       </div>
