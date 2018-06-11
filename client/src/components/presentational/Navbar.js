9019cbb81852b7057363513f3e3f40919e3026be
jportella93 <jportella93@gmail.com>
2018-06-11 10:05:04 +0200

    style: change nav buttons

--- client/src/components/presentational/Navbar.js
+++ client/src/components/presentational/Navbar.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
 import './Navbar.css'
 
 class Navbar extends Component {
-  // constructor (props) {
-  //   super(props);
-  //   this.state = {
-  //     hobbies : []
-  //   }
-  //   this.setHobbiesFromProps();
-  // }
+  constructor (props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  onRadioBtnClick = (rSelected, view) => {
+    this.setState({ rSelected });
+    this.props.changeView(view)
+  }
 
   render() {
     return (
       <div className="Navbar">
-        <button className="PostHobby" onClick={() => this.props.changeView('PostHobby')}
-          style={{'visibility': this.props.currentView === 'PostHobby'? 'hidden' : 'visible'}}
-        >Post Hobby</button>
 
-        <button className="Discover" onClick={() => this.props.changeView('Discover')}
-          style={{'visibility': this.props.currentView === 'Discover'? 'hidden' : 'visible'}}
-        >Dicover</button>
-
-        <button className="List" onClick={() => this.props.changeView('List')}
-          style={{'visibility': this.props.currentView === 'List'? 'hidden' : 'visible'}}
-        >List</button>
+        <ButtonGroup>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(1, 'PostHobby')} active={this.state.rSelected === 1}>Post</Button>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(2, 'Discover')} active={this.state.rSelected === 2}>Find</Button>
+          <Button color="primary" onClick={() => this.onRadioBtnClick(3, 'List')} active={this.state.rSelected === 3}>Liked</Button>
+        </ButtonGroup>
       </div>
     );
   }
